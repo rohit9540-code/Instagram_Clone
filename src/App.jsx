45f4742725a201cc23fc329c418c68dc2baae03f
rohit9540-code <rdxpandit111@gmail.com>
2024-11-03 05:04:27 +0530

    fixed firebase import error4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
-import PageLayout from "./Layouts/PageLayout/PageLayout";
+import PageLayout from "./layouts/PageLayout/PageLayout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase/firebase";
