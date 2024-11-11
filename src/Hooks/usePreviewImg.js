1c10ae6f4223c27fd5b5255bd250b7659f7351c4
rohit9540-code <rdxpandit111@gmail.com>
2024-11-12 01:50:48 +0530

    size for images to select increased

--- src/Hooks/usePreviewImg.js
+++ src/Hooks/usePreviewImg.js
@@ -4,7 +4,7 @@ import useShowToast from "./useShowToast";
 const usePreviewImg = () => {
 	const [selectedFile, setSelectedFile] = useState(null);
 	const showToast = useShowToast();
-	const maxFileSizeInBytes = 2 * 1024 * 1024; // 2MB
+	const maxFileSizeInBytes = 5 * 1024 * 1024; // 5MB
 
 	const handleImageChange = (e) => {
 		const file = e.target.files[0];
